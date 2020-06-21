251f7670f36f5504629a3da50589d6438929eb68
Georgy Sayganov <georgysay@gmail.com>
2020-06-21 14:13:41 +0200

    Add the index page Add locales

---
+++ src/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+@font-face {
+  font-family: 'JetBrainsMono';
+  src: url('https://cdn.jsdelivr.net/gh/JetBrains/JetBrainsMono/web/woff2/JetBrainsMono-Regular.woff2')
+      format('woff2'),
+    url('https://cdn.jsdelivr.net/gh/JetBrains/JetBrainsMono/web/woff/JetBrainsMono-Regular.woff')
+      format('woff'),
+    url('https://cdn.jsdelivr.net/gh/JetBrains/JetBrainsMono/ttf/JetBrainsMono-Regular.ttf')
+      format('truetype');
+}
+
+html,
+body {
+  background-color: #27282c;
+  font-family: 'JetBrainsMono';
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  margin: 0px !important;
+}
+</style>
